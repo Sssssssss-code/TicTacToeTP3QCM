87fcb3c1de8568b8548fa300fbecb2e88b590142
Solal Jourdan <solal.jourdan@laposte.net>
2025-12-05 14:56:27 +0100

    Adding the possibility to play on the grid

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import numpy as np
 
 class TicTacToe:
     def __init__(self) -> None:
-        self.grid=np.zeros((3,3))
+        self.grid=np.array([[-1 for _ in range(3)]for _ in range(3)])
+        self.cur_player=0
+    
+    def check_move_validity(self, movex, movey):
+        #to complete
+        pass
+    
+    def play_move(self,movex, movey):
+        if self.check_move_validity(movex, movey):
+            self.grid[movex, movey]=self.cur_player
+            self.cur_player=(self.cur_player+1)%2
 
         
